72ff5451a8104556f39391f198094ceb612c81b2
sreekruti <99178296+sreekruti@users.noreply.github.com>
2022-04-27 21:52:32 +0530

    Create print-positive-integers.py

---
+++ print-positive-integers.py
@@ -0,0 +1,13 @@
+
+list1=[12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+# iterating each number in list1
+print("LIST1 POSITIVE INTEGERS ARE: ")
+for nums1 in list1:
+    if nums1>= 0:
+        print( nums1, end = " ")
+# iterating each number in list2
+print("\nLIST2 POSITIVE INTEGERS ARE: ")
+for nums2 in list2:
+    if nums2>= 0:
+        print( nums2, end = " ")
