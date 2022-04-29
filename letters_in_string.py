e6e15815ee552dd2432d0b5924c6531ceee465b1
sreekruti <99178296+sreekruti@users.noreply.github.com>
2022-04-29 15:03:57 +0530

    Create letters_in_string.py

---
+++ letters_in_string.py
@@ -0,0 +1,10 @@
+def no_of_letters(string):
+    dict = {}
+    for n in string:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+print(no_of_letters('mississippi'))
