18806d795a9b6671ac864fdb8cef3de1022958e6
sreekruti <99178296+sreekruti@users.noreply.github.com>
2022-04-29 14:31:10 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -3,3 +3,10 @@ area = 3.14*r*r
 perimeter = 2*3.14*r
 print("Area of Circle: ",area)
 print("Perimeter of Circle: ",perimeter)
+
+#taking input of the filename and printing the extension of the filename
+fn= input("Enter a Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
