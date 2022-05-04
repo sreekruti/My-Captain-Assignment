5008f32ca169137d09d62dabf37962617e8bd55d
sreekruti <99178296+sreekruti@users.noreply.github.com>
2022-05-05 00:23:38 +0530

    Update letters_in_string.py

--- letters_in_string.py
+++ letters_in_string.py
@@ -1,10 +1,22 @@
-def no_of_letters(string):
-    dict = {}
-    for n in string:
-        keys = dict.keys()
-        if n in keys:
-            dict[n] += 1
-        else:
-            dict[n] = 1
-    return dict
-print(no_of_letters('mississippi'))
+text = 'Mississippi'
+
+
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
+
+
+def most_frequent(text):
+    letters = [letter.lower() for letter in text if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print (letter, count)
+
+most_frequent(text)
+
